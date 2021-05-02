86542fa1a4102600d96c1dd67d10eb24ffc04407
Eduardo Dini <eduardodiasdini@gmail.com>
2021-05-02 20:33:28 -0300

    Initial Commit

---
+++ PartyApp/PartyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyAppApp.swift
+//  PartyApp
+//
+//  Created by Eduardo Dini on 02/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PartyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
