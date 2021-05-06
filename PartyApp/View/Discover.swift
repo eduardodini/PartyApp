568941603dbe1cc3fdaa4b071dc2e5d6ec43ad77
Gabriel <bimuelas@gmail.com>
2021-05-06 12:28:22 -0300

    Navegação para EventView

--- PartyApp/View/Discover.swift
+++ PartyApp/View/Discover.swift
@@ -31,48 +31,51 @@ struct Discover_Previews: PreviewProvider {
 
 struct EventCard: View {
     var body: some View {
-        VStack {
-            Spacer()
-            HStack {
-                VStack {
-                    Text("Abr".uppercased())
-                        .foregroundColor(.purple)
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                    
-                    Text(String(format: "%02d", 1))
-                        .font(.headline)
-                }
-                .padding(.trailing, 10)
-                                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Copa BIXO 021")
-                        .font(.headline)
-                    
-                    HStack {
-                        Image(systemName: "mappin")
-                        Text("Unicamp - 2km")
-                    }
-                    .foregroundColor(Color(#colorLiteral(red: 0.6000000238418579, green: 0.6000000238418579, blue: 0.6000000238418579, alpha: 1)))
-                    .font(.caption)
-                }
-                
+        NavigationLink(destination: Event()) {
+            VStack {
                 Spacer()
-                
-                Button(action: {
-                    print("Curti")
-                }) {
-                    Image(systemName: "heart")
+                HStack {
+                    VStack {
+                        Text("Abr".uppercased())
+                            .foregroundColor(.purple)
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text(String(format: "%02d", 1))
+                            .font(.headline)
+                    }
+                    .padding(.trailing, 10)
+                                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Copa BIXO 021")
+                            .font(.headline)
+                        
+                        HStack {
+                            Image(systemName: "mappin")
+                            Text("Unicamp - 2km")
+                        }
+                        .foregroundColor(Color(#colorLiteral(red: 0.6000000238418579, green: 0.6000000238418579, blue: 0.6000000238418579, alpha: 1)))
+                        .font(.caption)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("Curti")
+                    }) {
+                        Image(systemName: "heart")
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
-                .buttonStyle(PlainButtonStyle())
+                .padding(EdgeInsets(top: 12, leading: 16, bottom: 12, trailing: 16))
+                .background(Color.white)
             }
-            .padding(EdgeInsets(top: 12, leading: 16, bottom: 12, trailing: 16))
-            .background(Color.white)
+            .frame(width: 254, height: 192)
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(14)
+            .shadow(color: .black.opacity(0.1), radius: 24, y: 4)
         }
-        .frame(width: 254, height: 192)
-        .background(Color.gray.opacity(0.2))
-        .cornerRadius(14)
-        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.1), radius: 24, y: 4)
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
