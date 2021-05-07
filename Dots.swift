62d5c0da1a48ce567bf712cf65ecb29d1c5af325
Eduardo Dini <eduardodiasdini@gmail.com>
2021-05-07 20:30:30 -0300

    Onboarding

---
+++ Dots.swift
@@ -0,0 +1,82 @@
+//
+//  sandbox.swift
+//  PartyApp
+//
+//  Created by Eduardo Dini on 07/05/21.
+//
+
+import SwiftUI
+
+//struct Dots: View {
+//    var pageIndex: Int
+//    
+//    var body: some View {
+//        VStack {
+//            switch pageIndex {
+//            case 1:
+//                Dots1()
+//            case 2:
+//                Dots2()
+//            case 3:
+//                Dots3()
+//            default:
+//                Text("Error")
+//            }
+//        }
+//        
+//    }
+//}
+//
+//struct Dots1: View {
+//    var body: some View {
+//        HStack{
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: .center)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//                .opacity(0.3)
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//                .opacity(0.3)
+//        }
+//    }
+//}
+//
+//struct Dots2: View {
+//    var body: some View {
+//        HStack{
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//                .opacity(0.3)
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: .center)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//                .opacity(0.3)
+//        }
+//    }
+//}
+//
+//struct Dots3: View {
+//    var body: some View {
+//        HStack{
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//                .opacity(0.3)
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: .center)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//                .opacity(0.3)
+//            Circle()
+//                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+//        }
+//    }
+//}
--- PartyApp/View/OnboardingView.swift
+++ PartyApp/View/OnboardingView.swift
@@ -10,22 +10,64 @@ import SwiftUI
 //TODO: implementar navegação entre os cards do onboarding
 
 struct OnboardingView: View {
+    @State var currentPageIndex = 0
+    
     var body: some View {
-        ThirdCard()
+        VStack {
+            Spacer()
+            switch currentPageIndex {
+            case 0:
+                FirstPage()
+            case 1:
+                SecondPage()
+            case 2:
+                ThirdPage()
+            default:
+                Text("Error")
+            }
+            Spacer()
+            if(currentPageIndex != 2){
+                HStack {
+                    Dots(pageIndex: currentPageIndex)
+                        .padding()
+                    Spacer()
+                    Button(action: {
+                        currentPageIndex += 1
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 21)
+                                .fill(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                                .frame(width: 120, height: 42)
+                            Text("Próximo")
+                                .font(.system(size: 20, weight: .medium))
+                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                .tracking(0.38)
+                                .multilineTextAlignment(.center)
+                        }
+                    }
+                    .padding()
+                }
+            } else {
+                HStack {
+                    Dots(pageIndex: currentPageIndex)
+                        .padding()
+                        .padding(.bottom)
+                    Spacer()
+                }
+            }
+            
+        }
     }
 }
 
-struct FirstCard: View {
+struct FirstPage: View {
     var body: some View {
+        //Title
         VStack {
             Text("Bem-Vindo")
                 .font(.system(size: 17, weight: .semibold))
-                .padding()
-            
-            //TODO: imagem
-            Circle()
-                .fill(Color(#colorLiteral(red: 0.7686274647712708, green: 0.7686274647712708, blue: 0.7686274647712708, alpha: 1)))
-                .frame(width: 340, height: 340)
+                .padding(.bottom, 70)
+            Image("party")
                 .padding()
             Text("Seus Eventos num só Lugar")
                 .font(.system(size: 22, weight: .semibold))
@@ -33,199 +75,243 @@ struct FirstCard: View {
             Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat est sapien libero risus mauris tortor. Tempor in neque sit ipsum vitae dolor.")
                 .font(.system(size: 17, weight: .light))
                 .padding()
-            
-            HStack{
-                //TODO: action dos buttons (skip, next)
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 21)
-                            .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                            .frame(width: 120, height: 42)
-                        Text("Skip")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                    }
-                }
-                
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 21)
-                            .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                            .frame(width: 120, height: 42)
-                        Text("Next")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-
-                    }
-                }
-            }
-                
         }
-        .padding()
+        .padding(20)
+
     }
 }
 
-struct SecondCard: View {
-    //FIXME: variavel criada para o código não quebrar
-    @State private var localizacao: String = " "
+struct SecondPage: View {
+    @State var localizacao = ""
     
     var body: some View {
         VStack {
-            //TODO: imagem
-            Circle()
-                .fill(Color(#colorLiteral(red: 0.7686274647712708, green: 0.7686274647712708, blue: 0.7686274647712708, alpha: 1)))
-                .frame(width: 277, height: 277)
-            
+            Image("party2")
+                .padding()
             Text("Encontre Eventos perto de você")
-                .font(.system(size: 28, weight: .semibold))
-
+                .font(.system(size: 22, weight: .semibold))
+                .padding()
             Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat est sapien libero risus mauris tortor.")
-                .font(.system(size: 17, weight: .regular))
+                .font(.system(size: 17, weight: .light))
+                .padding()
             
-            HStack {
-                Image(systemName: "mappin.and.ellipse")
-                //TODO: textField -> pesquisa/define localização
-                TextField("Localização", text: $localizacao)
-                    .textFieldStyle(DefaultTextFieldStyle())
-                    .disableAutocorrection(true)
-            }
-            .underlineTextField()
-            .padding()
-            
-            HStack {
-                
-                Image(systemName: "target")
-                //TODO: .onTapGesture -> pega localização atual
-                Text("Usar localização atual")
-                    .font(.system(size: 17, weight: .regular))
-                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0.48, blue: 1, alpha: 1)))
-
-            }
-            .padding()
-
-            //TODO: action do button (next)
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 21)
-                        .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .frame(width: 120, height: 42)
-                    Text("Next")
-                        .font(.system(size: 20, weight: .medium))
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+            VStack {
+                HStack {
+                    Image(systemName: "mappin.and.ellipse")
+                        .padding(.trailing)
+                    TextField("Localização", text: $localizacao)
                 }
+                .padding(.horizontal)
+                Divider()
+                    .padding(.horizontal, 40)
             }
+            
+            HStack {
+                Image(systemName: "scope")
+                    .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                Text("Usar localização atual")
+                    .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                Spacer()
+            }
+            .padding()
         }
         .padding(20)
     }
 }
 
-extension View {
-    func underlineTextField() -> some View {
-        self
-            .padding(.vertical, 10)
-            .overlay(Rectangle().frame(height: 2).padding(.top, 35))
-            .padding(10)
+struct ThirdPage: View {
+    
+    
+    var body: some View {
+        VStack{
+            
+            BlocoTexto()
+                .padding()
+            BlocoLogin()
+                .padding()
+            BlocoLoginAlternativo()
+                .padding()
+            
+            VStack {
+                Text("Não possui conta ainda?\n")
+                    .font(.system(size: 13, weight: .regular))
+                    .foregroundColor(Color(#colorLiteral(red: 0.21, green: 0.21, blue: 0.21, alpha: 1)))
+                Text("Cadastre-se")
+                    .font(.system(size: 13, weight: .regular))
+                    .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                    .padding(.top, -20)
+            }
+            
+            Text("Pular esta etapa")
+                .font(.system(size: 17, weight: .regular))
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                .padding()
+        
+        }
+//        .padding(.horizontal, 20)
     }
 }
 
-struct ThirdCard: View {
-    
-    //FIXME: variavel criada para o código não quebrar
-    @State private var localizacao: String = " "
-    
+
+
+
+struct BlocoTexto: View {
     var body: some View {
-        
-        VStack {
+        VStack(alignment: .leading) {
             Text("Acompanhe os eventos que seus amigos estão curtindo")
-                .font(.system(size: 28, weight: .semibold))
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-            
+                .font(.system(size: 22, weight: .semibold))
             Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat est sapien libero risus mauris tortor.")
-                .font(.system(size: 17, weight: .regular))
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-            
-            //TODO: textField -> email e senha
-            TextField("Email", text: $localizacao)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .disableAutocorrection(true)
-            
-            TextField("Senha", text: $localizacao)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .disableAutocorrection(true)
-            
+                .font(.system(size: 17, weight: .light))
+                .padding(.top)
+        }
+    }
+}
+
+struct BlocoLogin: View {
+    @State var strings = ""
+    var body: some View {
+        VStack {
+            VStack {
+                TextField("Email", text: $strings)
+                Divider()
+                    .padding(.horizontal, -20)
+            }
+            VStack {
+                TextField("Senha", text: $strings)
+                Divider()
+                    .padding(.horizontal, -20)
+            }
+            .padding(.bottom)
             Text("Esqueceu sua senha?")
                 .font(.system(size: 12, weight: .regular))
-                .foregroundColor(Color(#colorLiteral(red: 0, green: 0.48, blue: 1, alpha: 1)))
-
-            //TODO: entrar button
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+            Button(action: {
+                
+            }) {
                 ZStack {
                     RoundedRectangle(cornerRadius: 21)
-                        .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .frame(width: 299, height: 42)
+                        .fill(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                        .frame(width: 291, height: 42)
                     Text("Entrar")
                         .font(.system(size: 20, weight: .medium))
                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-            }
-            
-            }
-            ZStack {
-                //FIXME: Olhar no Figma como deve ficar
-                Divider()
-                Text("Ou entre com")
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(Color(#colorLiteral(red: 0.24, green: 0.24, blue: 0.26, alpha: 0.72)))
-            }
-            HStack {
-                //TODO: .onTapGesture Entrar com Apple/Google/Facebook
-                //FIXME: Adicionar símbolo do Google e do Facebook
-                ZStack {
-                    Circle()
-                        .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .frame(width: 42, height: 42)
-                    Text("")
-                        .font(.system(size: 23, weight: .medium))
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                }
-                ZStack {
-                    Circle()
-                        .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .frame(width: 42, height: 42)
-                    Text("")
-                        .font(.system(size: 23, weight: .medium))
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                }
-                
-                ZStack {
-                    Circle()
-                        .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .frame(width: 42, height: 42)
-                    Text("")
-                        .font(.system(size: 23, weight: .medium))
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .tracking(0.38)
+                        .multilineTextAlignment(.center)
                 }
             }
-
-            Text("Não possui conta ainda?\n")
-                .font(.system(size: 13, weight: .regular))
-                .foregroundColor(Color(#colorLiteral(red: 0.21, green: 0.21, blue: 0.21, alpha: 1))) +
-                //TODO: .onTapGesture -> cadastro
-                Text("Cadastre-se")
-                    .font(.system(size: 13, weight: .regular))
-                    .foregroundColor(Color(#colorLiteral(red: 0, green: 0.48, blue: 1, alpha: 1)))
-           
-            //TODO: onTapGesture -> Skip
-            Text("Pular esta etapa")
-                .font(.system(size: 17, weight: .regular))
-                .foregroundColor(Color(#colorLiteral(red: 0, green: 0.48, blue: 1, alpha: 1)))
-
         }
-        .padding()
+        .padding(.horizontal, 30)
+        
     }
 }
+
+
 
 struct Onboarding_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
+    }
+}
+
+struct BlocoLoginAlternativo: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .center) {
+                    Divider()
+                }
+                Text("Ou entre com")
+                    .font(.system(size: 12, weight: .regular))
+                    .foregroundColor(Color(#colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)))
+                    .background(Color.white)
+                VStack {
+                    Divider()
+                }
+            }
+            .padding()
+            HStack {
+                Image("apple")
+                    .clipShape(Circle())
+                Image("google")
+                    .clipShape(Circle())
+                Image("facebook")
+                    .clipShape(Circle())
+                
+            }
+        }
+    }
+}
+
+struct Dots: View {
+    var pageIndex: Int
+    
+    var body: some View {
+        VStack {
+            switch pageIndex {
+            case 0:
+                Dots1()
+            case 1:
+                Dots2()
+            case 2:
+                Dots3()
+            default:
+                Text("Error")
+            }
+        }
+        
+    }
+}
+
+struct Dots1: View {
+    var body: some View {
+        HStack{
+            Circle()
+                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+            Circle()
+                .frame(width: 10, height: 10, alignment: .center)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                .opacity(0.3)
+            Circle()
+                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                .opacity(0.3)
+        }
+    }
+}
+
+struct Dots2: View {
+    var body: some View {
+        HStack{
+            Circle()
+                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                .opacity(0.3)
+            Circle()
+                .frame(width: 10, height: 10, alignment: .center)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+            Circle()
+                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                .opacity(0.3)
+        }
+    }
+}
+
+struct Dots3: View {
+    var body: some View {
+        HStack{
+            Circle()
+                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                .opacity(0.3)
+            Circle()
+                .frame(width: 10, height: 10, alignment: .center)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+                .opacity(0.3)
+            Circle()
+                .frame(width: 10, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color(#colorLiteral(red: 0.64, green: 0.23, blue: 0.7, alpha: 1)))
+        }
     }
 }
